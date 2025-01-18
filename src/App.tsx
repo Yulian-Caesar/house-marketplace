c513f03515f433839921cc3d1837b0279b6e41f7
Yulian Khai <yulik19971997@gmail.com>
2025-01-18 18:11:34 +0200

    Firebase config, Pages and Routes, navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router"
+import { Explore as ExplorePage } from "./pages/Explore"
+import { ForgotPassword as ForgotPasswordPage } from "./pages/ForgotPassword"
+import { Offers as OffersPage } from "./pages/Offers"
+//import { Profile as ProfilePage } from "./pages/Profile"
+import { SignIn as SignInPage } from "./pages/SignIn"
+import { SignUp as SignUpPage } from "./pages/SignUp"
+import { Navbar } from "./components/Navbar"
+
 function App() {
 
 	return (
 		<>
-			<h2>Hello</h2>
+			<Router>
+				<Routes>
+					<Route path="/" element={<ExplorePage />} />
+					<Route path="/offers" element={<OffersPage />} />
+					<Route path="/profile" element={<SignInPage />} />
+					<Route path="/sign-in" element={<SignInPage />} />
+					<Route path="/sign-up" element={<SignUpPage />} />
+					<Route path="/forgot-password" element={<ForgotPasswordPage />} />
+				</Routes>
+				<Navbar />
+			</Router>
 		</>
 	)
 }
